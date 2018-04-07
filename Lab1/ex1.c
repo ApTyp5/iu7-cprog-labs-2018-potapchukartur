eebee063e395d3cb58dd9a0fbf8d9c1a2454dc7a
paa17u852 <aptyp_potatoch@mail.ru>
2018-04-07 03:51:07 +0300

    ex1.c: codestyle: 'spaces were deleted'

--- Lab1/ex1.c
+++ Lab1/ex1.c
@@ -15,7 +15,7 @@ int main()
     printf("Input coordinates of the points as in an example:\n");
     printf("<x1> <y1> <x2> <y2> <x3> <y3>\nYour input:");
 
-    scanf("%d %d %d %d %d %d", &x1, &y1, &x2, &y2, &x3, &y3);
+    scanf("%d%d%d%d%d%d", &x1, &y1, &x2, &y2, &x3, &y3);
     p = sqrt(pow(x1-x2,2) + pow(y1-y2,2));
     p += sqrt(pow(x2-x3,2) + pow(y2-y3,2));
     p += sqrt(pow(x3-x1,2) + pow(y3-y1,2));
