ce70528c8d9e1191955b0c37fee99acf0f69952b
paa17u852 <aptyp_potatoch@mail.ru>
2018-04-14 18:48:27 +0300

    Fib num function was improved

--- lab_02_2_1/main.c
+++ lab_02_2_1/main.c
@@ -1,28 +1,23 @@
 ﻿#include <stdio.h>
 #include <math.h>
 #include <limits.h>
-#define GOLDEN_RATIO (1 + sqrt(5))/2
 #define WRONG_INPUT -1
 #define TOO_BIG_NUM -2
 #define HAPPY_END 0
-#define MAX_NUM UINT_MAX
 
 
 unsigned int fib(unsigned int num)
 {
     // viv - ВЫВод
     unsigned int viv;
-    viv = (pow(GOLDEN_RATIO,num)/sqrt(5) + 0.5);
+    if (num > log((UINT_MAX - 0.5)*sqrt(5))/log((1+sqrt(5))/2))
+    {
+        printf("Too big number was inputed");
+	return TOO_BIG_NUM;
+    }
+    viv = (pow((1+sqrt(5))/2,num)/sqrt(5) + 0.5);
     return viv;
-}
-
-int max_serial_number()
-{
-    // viv - ВЫВод
-    unsigned int viv = 0;
-    viv = log((MAX_NUM - 0.5)*sqrt(5))/log(GOLDEN_RATIO);
-    return viv;
-}
+}[Ma@1
 
 
 int main()
@@ -41,15 +36,9 @@ int main()
         return WRONG_INPUT;
     }
 
-    max_ser_num = max_serial_number();
-
-    if (w > max_ser_num)
-    {
-        printf("Too big number was inputed.\n");
-        return TOO_BIG_NUM;
-    }
-
     viv = fib(w);
+    if (viv == TOO_BIG_NUM)
+	return TOO_BIG_NUM;
     printf("The Fib-number %u is: %u\n", w, viv);
 
     return HAPPY_END;
