21c871f786291487f0752f2467ab17b3338ddcd6
paa17u852 <aptyp_potatoch@mail.ru>
2018-04-21 20:23:15 +0300

    tabs ---> spaces

--- lab_04_2_1/main.c
+++ lab_04_2_1/main.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <math.h>
 
 #define N 10
@@ -14,78 +14,78 @@ int simple_prov(int num)
     for (int i = 2; i*i < num; i++)
     {
         if (!(num % (i)))
-			return NON_HAPPY_END;
+            return NON_HAPPY_END;
     }
 
-	return HAPPY_END;
+    return HAPPY_END;
 }
 
 // Копирует простые числа из массива "a" в массив "b"
 void copy_simple_numbers(int *a, int *b, int kvoa, int *kvob)
 {
-	
-	for (int i = 0; i < kvoa; i++)
-		if (!simple_prov(a[i]))
-			b[(*kvob)++] = a[i];
+    
+    for (int i = 0; i < kvoa; i++)
+        if (!simple_prov(a[i]))
+            b[(*kvob)++] = a[i];
 }
 
 
 int schet(int *a, char *invitation, int kvo)
 {
-	printf("%s",invitation);
-	for (int i = 0; i < kvo; i++)
-		if (!scanf("%d",a+i))
-			return WRONG_INPUT;
-		
-	return HAPPY_END;
+    printf("%s",invitation);
+    for (int i = 0; i < kvo; i++)
+        if (!scanf("%d",a+i))
+            return WRONG_INPUT;
+        
+    return HAPPY_END;
 }
 
 int main()
 {
-	FILE *f;
-	// Колличество элементов в массивах "А" и "В" 
-	int na,nb = 0;
-	int a[N], b[N]; 
-	
-	if (schet(&na, "Input the number of elements: ", 1))
-	{
-		printf("Error! The number of elements msut be a natural number.\n");
-		return WRONG_INPUT;
-	}
-	
-	
-	if (na < 0)
-	{
-		printf("Error! The number of elements msut be a natural number.\n");
-		return NEGATIVE_KVO;
-	}
-	
-	if (na > N)
-	{
-		printf("Error! The massive can contan maximum 10 elements.\n");
-		return TOO_MUCH_ELEMENTS;
-	}
-	
-	if (schet(a,"Input the elements of massive: ",na))
-	{
-		printf("Error! The elements of massive must be integer numbers.\n");
-		return WRONG_INPUT;
-	}
-	
-	copy_simple_numbers(a,b,na,&nb);
-	
-	f = fopen(FILENAME,"w");
-	fprintf(f,"b: ");
-	
-	printf("b: ");
-	
-	for (int i = 0; i < nb; i++)
-	{
-		printf("%d ", b[i]);
-		fprintf(f,"%d ", b[i]);
-	}
-	
-	fclose(f);
-	
-	return HAPPY_END;	
+    FILE *f;
+    // Колличество элементов в массивах "А" и "В" 
+    int na,nb = 0;
+    int a[N], b[N]; 
+    
+    if (schet(&na, "Input the number of elements: ", 1))
+    {
+        printf("Error! The number of elements msut be a natural number.\n");
+        return WRONG_INPUT;
+    }
+    
+    
+    if (na < 0)
+    {
+        printf("Error! The number of elements msut be a natural number.\n");
+        return NEGATIVE_KVO;
+    }
+    
+    if (na > N)
+    {
+        printf("Error! The massive can contan maximum 10 elements.\n");
+        return TOO_MUCH_ELEMENTS;
+    }
+    
+    if (schet(a,"Input the elements of massive: ",na))
+    {
+        printf("Error! The elements of massive must be integer numbers.\n");
+        return WRONG_INPUT;
+    }
+    
+    copy_simple_numbers(a,b,na,&nb);
+    
+    f = fopen(FILENAME,"w");
+    fprintf(f,"b: ");
+    
+    printf("b: ");
+    
+    for (int i = 0; i < nb; i++)
+    {
+        printf("%d ", b[i]);
+        fprintf(f,"%d ", b[i]);
+    }
+    
+    fclose(f);
+    
+    return HAPPY_END;   
 }
