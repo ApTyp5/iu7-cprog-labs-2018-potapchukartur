fbe75b1fe5c2022ffbef9d9668fbdbb8c375bddc
paa17u852 <aptyp_potatoch@mail.ru>
2018-05-19 19:11:20 +0300

    solving rep bug

--- lab_06/lab_06_2_1/major-work-function.c
+++ lab_06/lab_06_2_1/major-work-function.c
@@ -2,20 +2,6 @@
 #include <errno.h>
 #include <string.h>
 
-#define N 100
-#define HAPPY_END 0
-#define NON_HAPPY_END -1
-#define FORMAT_ERROR -2
-#define OPEN_INPUT_FILE_ERROR -3
-#define OPEN_OUTPUT_FILE_ERROR -4
-#define INCORRECT_FILE -5
-#define CLOSE_INPUT_FILE_ERROR -6
-#define CLOSE_OUTPUT_FILE_ERROR -7
-#define SAVE_ERROR -8
-#define READING_ERROR -9
-#define EMPTY_FILE -10
-
-
 
 int major_work(int *sl, const int *const el)
 {
