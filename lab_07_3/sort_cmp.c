7a4470d3c09b144b80d4143b5872431b04acd8d0
R2r <aptyp.potatoch@gmail.com>
2018-09-16 14:25:57 +0300

    Now we're able to vompare sorts

--- lab_07_3/sort_cmp.c
+++ lab_07_3/sort_cmp.c
@@ -6,6 +6,7 @@
 
 
 
+#define		SORT0_FILE		"trans.inv"
 #define		SORT1_FILE		"bp.inv"
 #define		SORT2_FILE		"qs.inv"
 #define		SORT1_NAME		"binPastes"
@@ -14,12 +15,14 @@
 #define		BEG			10
 #define		END			1000000
 #define		STEP			10000
-
+#define		numOfIter		10
 
 
 #define		START_MEASURE		tm = clock();
 #define		END_MEASURE(T)		T = (clock() - tm);
-#define		___PART(a)		printf("%s\n", a); 
+
+
+
 
 
 size_t tm;
@@ -29,81 +32,105 @@ size_t tm;
 int *incArr(int);
 int *decArr(int);
 int *ranArr(int);
-void compareSorts(char *comment, \
+void compareSorts(char *comment1, char *comment2, \
 		void sort1(void *, void *, int , int (*)(const void *, const void *)), \
 		void sort2(void *, size_t, size_t, int (*)(const void *,const void *)), \
 		int *createArr(int), int size_min, int size_max, \
-		int step, FILE *f1, FILE *f2);
+		int step, char *fname1, char *fname2);
 
 
 
 
 int main(int argc, char **argv)
 {
-	FILE *f1 = fopen(SORT1_FILE, "w");
-	FILE *f2 = fopen(SORT2_FILE, "w");
-
-	fprintf(f1, "%s\n\n\n", SORT1_NAME);
-	fprintf(f2, "%s\n\n\n", SORT2_NAME);
 
 
-	compareSorts("Inc 10.100.10", mysort, qsort, incArr, 10, 100, 10, f1, f2);
-	compareSorts("Ran 10.100.10", mysort, qsort, ranArr, 10, 100, 10, f1, f2);
-	compareSorts("Dec 10.100.10", mysort, qsort, decArr, 10, 100, 10, f1, f2);
-
-	compareSorts("Inc 1000.1001.10", mysort, qsort, incArr, 1000, 1100, 10, f1, f2);
-	compareSorts("Ran 1000.1001.10", mysort, qsort, ranArr, 1000, 1100, 10, f1, f2);
-	compareSorts("Dec 1000.1001.10", mysort, qsort, decArr, 1000, 1100, 10, f1, f2);
-
-	compareSorts("Inc 1000 000.1000 001.10", mysort, qsort, incArr, 10000, 10100, 10, f1, f2);
-	compareSorts("Ran 1000 000.1000 001.10", mysort, qsort, ranArr, 10000, 10100, 10, f1, f2);
-	compareSorts("Dec 1000 000.1000 001.10", mysort, qsort, decArr, 10000, 10100, 10, f1, f2);
+	compareSorts("BP", "QS", mysort, qsort, ranArr, 10, 500 , 10, "trans.inv", NULL);
+	compareSorts("BP", "QS", mysort, qsort, ranArr, 10, 5000, 10, "bp.inv", "qs.inv");
 
 
 	return 0;
 }
 
 
-void compareSorts(char *comment, \
+void compareSorts(char *comment1, char *comment2, \
 		void sort1(void *, void *, int , int (*)(const void *, const void *)), \
 		void sort2(void *, size_t, size_t, int (*)(const void *, const void *)), \
 		int *createArr(int), int size_min, int size_max, \
-		int step, FILE *f1, FILE *f2)
+		int step, char *fname1, char *fname2)
 {
-	fprintf(f1, "%s\n", comment);
-	fprintf(f2, "%s\n", comment);
+	FILE *f1 = fopen(fname1, "w");
+	FILE *f2;
+
+	char fstr[7] = "%10ld\n\0";
+	if (fname2 == NULL)
+	{
+		fstr[5] = '\t';
+		f2 = f1;
+		fprintf(f1, "%s\t%s\n", comment1, comment2);
+	}
+	else
+	{
+		f2 = fopen(fname2, "w");
+		fprintf(f1, "%s\n", comment1);
+		fprintf(f2, "%s\n", comment2);
+	}
+	     	
 
 
-	double tacts;
+
+	size_t tacts = 0;
+	size_t sum = 0;
 
 	for (int i = size_min; i <= size_max; i += step)
 	{
-		printf("i = %d\n", i);
-		int *pa = createArr(i);
-		int *epa = pa + i;
+		printf("Iteration %d\n", i);
+		int *pa;
+		int *epa;
+	       
+		
+		for (int j = 0; j < numOfIter; j++)
+		{
+			pa = createArr(i);
+			epa = pa + i;
 
-		START_MEASURE
+			START_MEASURE
 
-		sort1(pa, epa, sizeof(int), intComp);
+			sort1(pa, epa, sizeof(int), intComp);
 
-		END_MEASURE(tacts)
+			END_MEASURE(tacts)
 
-		fprintf(f1, "%d : %10lf\n", i, tacts);
-		free(pa);
+			sum += tacts;
+			
+			free(pa);
+		}
+
+		fprintf(f1, fstr, sum/numOfIter);
+
 		
 
+		sum = 0;
 
-		pa = createArr(i);
+		for (int j = 0; j < numOfIter; j++)
+		{
+			pa = createArr(i);
 
-		START_MEASURE
+			START_MEASURE
 
-		sort2(pa, i, sizeof(int), intComp);
+			sort2(pa, i, sizeof(int), intComp);
 
-		END_MEASURE(tacts)
+			END_MEASURE(tacts)
 
-		fprintf(f2, "%d : %10lf\n", i, tacts);
-		free(pa);
+			sum += tacts;
+			
+			free(pa);
+		}
+		fprintf(f2, "%10ld\n", sum/numOfIter);
 	}
+
+	fclose(f1);
+	if (fname2 != NULL)
+		fclose(f2);
 }
 
 
