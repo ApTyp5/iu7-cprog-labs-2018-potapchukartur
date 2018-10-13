213a27eb4a41702fe5310bc1144a2d55dc49d726
R2r <aptyp.potatoch@gmail.com>
2018-10-13 10:14:50 +0300

    tests modifide

--- lab_08_2_21/action.c
+++ lab_08_2_21/action.c
@@ -7,13 +7,6 @@
 #include "action.h"
 #include "Ghauss.h"
 
-int mult(matrix_t mtr1, int m1len, int m1wid,
-    matrix_t mtr2, int m2len, int m2wid,
-    matrix_t *ans, int *anslen, int *answid);
-
-int add(matrix_t mtr1, int m1len, int m1wid,
-    matrix_t mtr2, int m2len, int m2wid,
-    matrix_t *ans, int *anslen, int *answid);
 
 
 int act(str_t action, matrix_t mtr1, int m1len, int m1wid,
--- lab_08_2_21/test.c
+++ lab_08_2_21/test.c
@@ -5,30 +5,48 @@
 int main()
 {
 //    taction();
+
+    tadd_1();//pos
+    tadd_2();//neg
+
+    tmult_1();
+    tmult_2();
+
+    tact_1();//add pos
+    tact_2();//add neg
+    tact_3();//mult pos
+    tact_4();//mult neg
+    tact_5();//Ghauss pos
+    tact_6();//Ghauss neg
+
+
 //    tGhauss();
+
+    tGhauss_1();//pos
+    tGhauss_2();//neg
+
+    tcol_method_max_diag();
+
+    tchange_ptrs();
+
+    tchange_cols();
+
+    tchange_els();
+
+    tadd_vectors();
+
+    tcount_ans();
+
 //    tprompt();
+
+    
+
 //    tinout();
+
+
 
     return 0;
 }
-
-
-
-void taction()
-{
-//    tadd_1();
-//    tadd_2();
-
-//    tmult_1();
-//    tmult_2();
-}
-
-
-void tGhauss()
-{
-
-}
-
 
 
 
