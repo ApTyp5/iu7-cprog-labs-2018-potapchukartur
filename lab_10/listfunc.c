a6761a223bde7d4580ec67e64dc74914ae27bca2
paa17u852 <aptyp.potatoch@gmail.com>
2018-11-29 18:07:57 +0300

    sorted_merge improved

--- lab_10/listfunc.c
+++ lab_10/listfunc.c
@@ -167,6 +167,9 @@ void front_back_split(node_t *head, node_t **back)
 node_t *sorted_merge(node_t **head_a, node_t **head_b,
     int comparator(const void *, const void *))
 {
+    if (head_a == NULL || head_b == NULL)
+        return NULL;
+
     node_t *cur_a = *head_a;
     node_t *cur_b = *head_b;
 
@@ -215,6 +218,15 @@ node_t *sorted_merge(node_t **head_a, node_t **head_b,
 
     return result;
 }
+
+
+int list_extend();
+
+
+int comp_add_front();
+
+
+
 
 
 void free_list(node_t *head)
